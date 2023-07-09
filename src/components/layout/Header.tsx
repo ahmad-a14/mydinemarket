ebb43d8b0bc7d83a3fd0915125b43ba1925a9b1b
Ahmad Ali <107180396+ahmad-a14@users.noreply.github.com>
2023-07-09 12:57:30 +0500

    Header done

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import logo from '/public/logo.webp'
 import Image from 'next/image'
+import Link from 'next/link'
+import {ShoppingCart} from "lucide-react"
+import { Input } from "@/components/ui/input"
 
 export default function Header() {
   return (
-    <div>
-        <Image src={logo} alt="Logo" />
+    <div className="flex bg-slate-700 justify-between items-center py-6 px-8" >
+        <Image  src={logo} alt="Logo" className="w-40" />
+        <ul className="flex gap-x-10 ">
+            <li  className="text-lg"><Link href={"/"}>Female</Link> </li>
+            <li  className="text-lg"><Link href={"/"}>Male</Link></li>
+            <li  className="text-lg"><Link href={"/"}>Kids</Link></li>
+            <li  className="text-lg"><Link href={"/"}>All products</Link></li>
+        </ul>
+        <Input placeholder="Search" className="w-30 h-8" />
+        <div className="h-10 w-10 rounded-full bg-gray-300 flex justify-center items-center"> <ShoppingCart className="h-6 w-6" /> </div>
     </div>
   )
 }
