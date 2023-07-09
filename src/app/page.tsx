5018b15f3a334b5117d5a3e4691525d23364f00f
Ahmad Ali <107180396+ahmad-a14@users.noreply.github.com>
2023-07-09 13:17:25 +0500

    Hero started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button"
+import Hero from "@/views/Hero"
 
 
 export default function Home() {
   return (
     <div>
-      
+      <Hero />
     </div>
      
   )
--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Badge } from '@/components/ui/badge'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section>
+        {/* Left div */}
+            <div>
+                <Badge className="py-3 px-6 rounded-lg bg-blue-200 text-purple-700 hover:bg-blue-200"> Sale </Badge>
+            </div>
+        {/* RIght div */}
+    </section>
   )
 }
 
